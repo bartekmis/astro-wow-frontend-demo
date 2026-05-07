6b9e50f7f9bb6b28d79d9d3e724d215fe137fb37
Bartek Mis <bartek@studiosidekicks.com>
2026-05-07 21:12:45 +0200

    astro ISR added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import preact from '@astrojs/preact';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
+
   vite: {
     plugins: [tailwindcss()]
   },
